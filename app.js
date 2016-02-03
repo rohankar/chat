555e8c669ca58ffe673bfbcab897201dec0f5478
Rohan <rohankar@live.com>
2016-02-03 23:51:19 +0530

    socket.io server initialized

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ app.listen(1337, function(req, res){
 
 	console.log('listining to port 1337')
 });
+
+
+/*var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
+});
+server.listen(3000);*/
---
+++ server.js
@@ -0,0 +1,4 @@
+var io = require('socket.io')();
+// or 
+var Server = require('socket.io');
+var io = new Server();
